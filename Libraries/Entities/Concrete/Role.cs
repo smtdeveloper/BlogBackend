efb7747de0b3bf59eb444be471db221835c62c63
Mislina Aydın <75722301+mislinaaydn@users.noreply.github.com>
2021-08-09 01:11:42 +0300

    Entities katmanının ve user ın eksikleri giderildi

---
+++ Libraries/Entities/Concrete/Role.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities.Abstract;
+using System;
+
+namespace Entities.Concrete
+{
+   public class Role :IEntity
+    {
+        public Guid Id { get; set; }
+        public Guid SecondaryId { get; set; }
+        public string FilePath { get; set; }
+        public  bool IsDeleted { get; set; }
+    }
+}
